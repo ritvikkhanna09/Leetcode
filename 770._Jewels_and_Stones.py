20cebe28cea471afba00ba7700064bfa70b2c60a
Ritvik Khanna <ritvikkhanna09@gmail.com>
2018-03-12 11:00:16 +0530

    '12/03/18'

--- 770._Jewels_and_Stones.py
+++ 770._Jewels_and_Stones.py
@@ -6,3 +6,17 @@ class Solution:
                 if i==S[j]:
                     sum+=1
         return sum
+
+
+
+"""
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        int res = 0;
+        set<char> setJ(J.begin(), J.end());
+        for (char s : S) if (setJ.count(s)) res++;
+        return res;
+    }
+};
+"""
