e16d0ca981c67037147693ce9396fd549314857f
Ritvik Khanna <ritvikkhanna09@gmail.com>
2018-03-25 23:44:49 +0530

    '25/03/18'

---
+++ 605._Can_Place_Flowers.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        count = 0
+        flowerbed = [0] + flowerbed + [0]
+        for i in range(1, len(flowerbed)-1):
+        	if flowerbed[i-1] == flowerbed[i] == flowerbed[i+1] == 0:
+        		flowerbed[i] = 1
+        		count += 1
+        return count >= n
