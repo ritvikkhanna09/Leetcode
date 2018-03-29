2c2f9cd06c2b968abc9217e8cd9b132a4b017798
Ritvik Khanna <ritvikkhanna09@gmail.com>
2018-03-29 20:05:34 +0530

    '29/03/18'

---
+++ 566._Reshape_the_Matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+        int init_row = nums.size(), init_col = nums[0].size();
+        if (m * n != r * c) {
+            return nums;
+        }
+        vector<vector<int>> rev(r, vector<int>(c, 0));
+        for (int i = 0; i < init_row; i++) {
+            for (int j = 0; j < init_col; j++) {
+                int k = i * init_col + j;
+                res[k / c][k % c] = nums[i][j];
+            }
+        }
+
+        return res;
+    }
+};
+
