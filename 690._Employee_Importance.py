fa07ff1bc3adeefe6cc4df0983cef2b48ece79c8
Ritvik Khanna <ritvikkhanna09@gmail.com>
2018-04-05 21:52:32 +0530

    '5/04/18'

---
+++ 690._Employee_Importance.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def getImportance(self, employees, id):
+        emps = {employee.id: employee for employee in employees}
+        def dfs(id):
+            subordinates_importance = sum([dfs(sub_id) for sub_id in emps[id].subordinates])
+            return subordinates_importance + emps[id].importance
+        return dfs(id)
