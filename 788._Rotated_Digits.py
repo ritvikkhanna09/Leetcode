e04523d2ffea4591115c52e5aea3621119bbdf0c
Ritvik Khanna <ritvikkhanna09@gmail.com>
2018-04-07 23:30:38 +0530

    '7/4/18'

--- 788._Rotated_Digits.py
+++ 788._Rotated_Digits.py
@@ -1,7 +1,5 @@
 def rotatedDigits(self, N):
-        s1 = set([1, 8, 0])
-        s2 = set([1, 8, 0, 6, 9, 2, 5])
-        def isGood(x):    
-            s = set([int(i) for i in str(x)])
-            return s.issubset(s2) and not s.issubset(s1)
-        return sum(isGood(i) for i in range(N + 1))
+    def isValidRotation(num):
+        return (not any(s in {'3', '4', '7'} for s in num)) and any(s in {'2', '5', '6', '9'} for s in num)
+
+    return sum([isValidRotation(str(i)) for i in range(1, N+1)])
