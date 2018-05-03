f083fc140bd9c3de34086777240101a4512fc2b8
Ritvik <ritvikkhanna09@gmail.com>
2018-05-03 22:55:46 +0530

    3/05/18

---
+++ 617._Merge_Two_Binary_Trees.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (t1==nullptr)
+            return t2;
+        if (t2==nullptr)
+            return t1;
+        struct TreeNode* t3=new TreeNode(t1->val+t2->val);
+        t3->left=mergeTrees(t1->left,t2->left);
+        t3->right=mergeTrees(t1->right,t2->right);
+        return t3;   
+    }
+};
+
