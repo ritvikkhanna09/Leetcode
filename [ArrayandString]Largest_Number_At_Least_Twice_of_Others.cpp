ad6a9c221325ca99fd3101dfd1bbd1b2a414df29
Ritvik <ritvikkhanna09@gmail.com>
2018-05-05 13:09:57 +0530

    5/05/18

---
+++ [ArrayandString]Largest_Number_At_Least_Twice_of_Others.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums){
+        int maxIndex = 0;
+        for (int i = 0; i < nums.size(); ++i){
+            if (nums[i] > nums[maxIndex])
+                maxIndex = i;
+        }
+        
+        for (int i = 0; i < nums.size(); ++i){             
+            if (maxIndex != i && nums[maxIndex] < 2 * nums[i])
+                return -1;
+        }
+     
+        return maxIndex;
+    }
+};
