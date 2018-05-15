9a600576b7c1f27d9a119ac4010f1c3e23c44421
Ritvik <ritvikkhanna09@gmail.com>
2018-05-15 23:22:02 +0530

    15/05/18

--- 788._Rotated_Digits.py
+++ 788._Rotated_Digits.py
@@ -1,5 +1,19 @@
-def rotatedDigits(self, N):
-    def isValidRotation(num):
-        return (not any(s in {'3', '4', '7'} for s in num)) and any(s in {'2', '5', '6', '9'} for s in num)
-
-    return sum([isValidRotation(str(i)) for i in range(1, N+1)])
+class Solution:
+    def rotatedDigits(self, N):
+        count=0
+        dictrot={'0':'0','1':'1','2':'5','3':'3','4':'4','5':'2','6':'9','7':'7','8':'8','9':'6'}
+        for digit in range(1,N+1):
+            flag=0
+            # print("ont this digit::",digit)
+            str_temp=str(digit)
+            rot=""
+            for number in str_temp:
+                if number not in ['2','1','0','5','6','8','9']:
+                    flag=1
+                rot+=dictrot[number]
+            # print("rot::",rot)
+            if(rot!=str_temp and flag==0):
+                count+=1
+            # print("count::",count)
+        return count
+        
