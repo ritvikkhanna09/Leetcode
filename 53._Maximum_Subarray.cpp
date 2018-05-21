9d9da41c2cfc94e50d59573a82a07a3ef955c031
Ritvik <ritvikkhanna09@gmail.com>
2018-05-22 00:37:03 +0530

    22/05/18

--- 53._Maximum_Subarray.cpp
+++ 53._Maximum_Subarray.cpp
@@ -1,1 +1,11 @@
-53. Maximum Subarray
+class Solution {
+public:
+    int maxSubArray(vector<int>& A) {
+        int maxSoFar=A[0], maxEndingHere=A[0];
+        for (int i=1;i<A.size();++i){
+    	    maxEndingHere= max(maxEndingHere+A[i],A[i]);
+    	    maxSoFar=max(maxSoFar, maxEndingHere);	
+        }
+        return maxSoFar;
+    }
+};
