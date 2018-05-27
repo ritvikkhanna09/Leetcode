27ad11800dd0ef9dbf1cf27bcb61fc6c12e588d3
Ritvik <ritvikkhanna09@gmail.com>
2018-05-27 11:04:13 +0530

    27/05/18

---
+++ 300._Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(),1);
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j] && dp[i]<dp[j]+1)
+                    dp[i]++;
+            }
+        }
+        int max=0;
+        for(int i=0;i<dp.size();i++){
+            if(max<dp[i])
+                max=dp[i];
+        }
+        return max;
+    }
+};
