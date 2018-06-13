fd9c5b51617971c5d8fcaa84fd6ad6e84c9d7fb1
Ritvik <ritvikkhanna09@gmail.com>
2018-06-13 14:28:57 +0530

    13/06/18

---
+++ 88._Merge_Sorted_Array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> temp;
+        int i,j;
+        for(i=0,j=0;i<m &&j<n;){
+            if(nums1[i]<nums2[j]){
+                temp.push_back(nums1[i]);
+                i++;
+            }else{
+                temp.push_back(nums2[j]);
+                j++; 
+            }
+        }
+        while(i<m){
+            temp.push_back(nums1[i]);
+            i++;
+        }
+        while(j<n){
+            temp.push_back(nums2[j]);
+            j++;
+        }
+        nums1=temp;
+    }
+};
