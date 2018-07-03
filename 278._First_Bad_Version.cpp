78985e8563895d6ebde37cb1eed1ffbfd8c40340
Ritvik <ritvikkhanna09@gmail.com>
2018-07-03 16:17:40 +0530

    03/07/18

---
+++ 278._First_Bad_Version.cpp
@@ -0,0 +1,17 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low=0,high=n,mid;
+        while(low<high){
+            mid=low+(high-low)/2;
+            if(isBadVersion(mid))
+                high=mid;
+            else
+                low=mid+1;
+        }
+        return low;
+    }
+};
